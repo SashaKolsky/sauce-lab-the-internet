6f6c307119b87c354369c71b33038239fd668855
Alexander Kazakov <alexander@kazakov.mobi>
2023-01-23 20:55:34 -0800

    Removes leftover @FindAll annonation that was breaking the test execution.

--- src/test/java/com/dotdash/takehome/pages/TheInternetHomePage.java
+++ src/test/java/com/dotdash/takehome/pages/TheInternetHomePage.java
@@ -2,18 +2,12 @@ package com.dotdash.takehome.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import java.util.List;
 
 public class TheInternetHomePage extends BasePage {
 
     public static final String PAGE_URL = "http://localhost:7080";
-
-    @FindAll({@FindBy(partialLinkText = "Context Menu"),
-              @FindBy(partialLinkText = "Digest Authentication")})
 
     @FindBy(partialLinkText = "Form Authentication")
     WebElement loginPageLink;
