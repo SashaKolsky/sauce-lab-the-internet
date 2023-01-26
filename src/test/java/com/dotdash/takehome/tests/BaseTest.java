11c8f692ec42cc51cd197ca480ab3f5a8540af64
Alexander Kazakov <alexander@kazakov.mobi>
2023-01-25 18:02:17 -0800

    Adds test runtime to log as log info.

--- src/test/java/com/dotdash/takehome/tests/BaseTest.java
+++ src/test/java/com/dotdash/takehome/tests/BaseTest.java
@@ -6,10 +6,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public abstract class BaseTest {
 
+    static final Logger log = getLogger(lookup().lookupClass());
     private WebDriver driver;
+    static private long startTime;
+    static private long endTime;
+
+    @BeforeAll
+    static void beforeClass() {
+        startTime = System.nanoTime();
+    }
 
     @BeforeEach
     public void setup() {
@@ -27,7 +42,14 @@ public abstract class BaseTest {
         }
     }
 
-    public WebDriver getDriver() {
+    @AfterAll
+    static void afterClass() {
+        endTime = System.nanoTime();
+        long runtime = TimeUnit.NANOSECONDS.toSeconds(endTime - startTime);
+        log.atInfo().log("Runtime for all tests in class: "+ runtime);
+    }
+
+    protected WebDriver getDriver() {
         return driver;
     }
 }
