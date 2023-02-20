249bdfb8f2262429ac41096987f84936e9462012
Alexander Kazakov <alexander@kazakov.mobi>
2023-02-20 15:53:09 -0800

    feat: add readFileContent helper method for loading file content as a string

--- src/test/java/com/dotdash/takehome/utils/Utils.java
+++ src/test/java/com/dotdash/takehome/utils/Utils.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -27,6 +28,45 @@ public class Utils {
         }
 
         return "";
+    }
+
+    public static String readFileContents(String fileName) {
+
+        URI filePath;
+        try {
+            filePath = Utils.class.getResource(fileName).toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Create a StringBuilder object to store the contents of the file
+        StringBuilder contents = new StringBuilder();
+
+        try {
+            // Create a File object
+            File file = new File(filePath);
+
+            // Create a FileReader object
+            FileReader fileReader = new FileReader(file);
+
+            // Wrap the FileReader object in a BufferedReader
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            // Read each line of the file
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                contents.append(line).append("\n");
+            }
+
+            // Close the BufferedReader
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filePath);
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + filePath);
+        }
+
+        return contents.toString();
     }
 
 }
