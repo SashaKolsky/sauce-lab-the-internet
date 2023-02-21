de3f66f5948989710723f1da5f8dd8cb8768527d
Alexander Kazakov <alexander@kazakov.mobi>
2023-02-20 18:54:50 -0800

    feat: add / change to webdriver waiter methods

--- src/test/java/com/dotdash/takehome/pages/BasePage.java
+++ src/test/java/com/dotdash/takehome/pages/BasePage.java
@@ -2,6 +2,7 @@ package com.dotdash.takehome.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,19 +13,23 @@ public class BasePage {
     protected WebDriver driver;
     private static final int TIMEOUT = 5;
     private static final int POLLING = 100;
-    private WebDriverWait wait;
+    private final WebDriverWait wait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT), Duration.ofMillis(POLLING));
     }
 
-    protected void waitForElementToAppear(By locator) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    protected void waitForElementToAppear(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 
-    protected void waitForElementToDisappear(By locator) {
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    protected void waitForElementToBeClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    protected void waitForElementToDisappear(WebElement element) {
+        wait.until(ExpectedConditions.invisibilityOf(element));
     }
 
     protected void waitForTextToDisappear(By locator, String text) {
